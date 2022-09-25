b2a69e2f04259e9563e6977fc470d712ebb40069
Dinh Lam <dinhlamvn353@gmail.com>
2022-09-25 19:06:57 +0700

    show password alias in confirm dialog

--- app/src/main/java/com/dinhlam/sharesaver/ui/dialog/folder/confirmpassword/FolderConfirmPasswordDialogFragment.kt
+++ app/src/main/java/com/dinhlam/sharesaver/ui/dialog/folder/confirmpassword/FolderConfirmPasswordDialogFragment.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
+import com.dinhlam.sharesaver.R
 import com.dinhlam.sharesaver.base.BaseDialogFragment
 import com.dinhlam.sharesaver.databinding.DialogFolderConfirmPasswordBinding
 import com.dinhlam.sharesaver.extensions.cast
 import com.dinhlam.sharesaver.extensions.getTrimmedText
+import com.dinhlam.sharesaver.extensions.takeIfNotNullOrBlank
 import com.dinhlam.sharesaver.utils.ExtraUtils
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -38,6 +41,19 @@ class FolderConfirmPasswordDialogFragment :
     override fun onViewDidLoad(view: View, savedInstanceState: Bundle?) {
         val folderId: String = arguments?.getString(ExtraUtils.EXTRA_FOLDER_ID) ?: return
         viewModel.loadFolderData(folderId)
+
+        viewModel.consumeOnChange(FolderConfirmPasswordDialogData::folder) { folder ->
+            val nonNull = folder ?: return@consumeOnChange
+            nonNull.passwordAlias.takeIfNotNullOrBlank()?.let { alias ->
+                viewBinding.textPasswordAlias.visibility = View.VISIBLE
+                viewBinding.textPasswordAlias.text = HtmlCompat.fromHtml(
+                    getString(R.string.password_alias, alias), HtmlCompat.FROM_HTML_MODE_LEGACY
+                )
+            } ?: viewBinding.textPasswordAlias.run {
+                text = null
+                visibility = View.GONE
+            }
+        }
 
         viewModel.consumeOnChange(FolderConfirmPasswordDialogData::error) { errorRes ->
             if (errorRes != 0) {
