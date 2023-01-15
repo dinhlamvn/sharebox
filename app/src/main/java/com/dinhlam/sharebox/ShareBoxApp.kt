bac426c2b6fac398e98b685353242271cccf7baf
Dinh Lam <dinhlamvn353@gmail.com>
2023-01-15 10:42:16 +0700

    fix menu icon

--- app/src/main/java/com/dinhlam/sharebox/ShareBoxApp.kt
+++ app/src/main/java/com/dinhlam/sharebox/ShareBoxApp.kt
@@ -1,14 +1,9 @@
 package com.dinhlam.sharebox
 
-import android.R
 import android.app.Application
-import android.widget.Toast
-import com.dinhlam.sharebox.logger.Logger
 import com.dinhlam.sharebox.pref.AppSharePref
 import com.dinhlam.sharebox.repository.FolderRepository
 import com.dinhlam.sharebox.utils.FolderUtils
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
@@ -30,29 +25,16 @@ class ShareBoxApp : Application() {
     override fun onCreate() {
         super.onCreate()
         createDefaultFoldersOnFirstLaunch()
-
-        syncFirebaseToken()
     }
 
     private fun createDefaultFoldersOnFirstLaunch() {
         if (!appSharePref.isAppFirstLaunch()) {
             applicationScope.launch(Dispatchers.IO) {
-                folderRepository.insertMany(*FolderUtils.getDefaultFolders(this@ShareBoxApp).toTypedArray())
+                folderRepository.insertMany(
+                    *FolderUtils.getDefaultFolders(this@ShareBoxApp).toTypedArray()
+                )
                 appSharePref.commitAppFirstLaunch()
             }
         }
-    }
-
-    private fun syncFirebaseToken() {
-        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
-            if (!task.isSuccessful) {
-                Logger.warning("Fetching FCM registration token failed", task.exception)
-                return@OnCompleteListener
-            }
-
-            // Get new FCM registration token
-            val token = task.result
-            Logger.debug("Here you token: $token")
-        })
     }
 }
