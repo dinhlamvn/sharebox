456ea7b1958a6f5cfc9d9e6d8a4203b56c5a8edd
Dinh Lam <dinhlamvn353@gmail.com>
2023-02-14 14:21:28 +0700

    update default folder

--- app/src/main/java/com/dinhlam/sharebox/utils/FolderUtils.kt
+++ app/src/main/java/com/dinhlam/sharebox/utils/FolderUtils.kt
@@ -13,7 +13,10 @@ object FolderUtils {
             FOLDER_HOME_ID,
             context.getString(R.string.default_folder_name),
             context.getString(R.string.default_folder_desc)
-        )
+        ),
+        Folder("tiktok_video", "Tiktok", "Video from Tiktok"),
+        Folder("youtube_short", "Youtube Shorts", "Video from Youtube Shorts"),
+        Folder("facebook_reels", "Facebook Reels", "Video from Facebook Reels"),
     )
 
     fun isProtectedFolder(id: String): Boolean {
--- app/src/main/java/com/dinhlam/sharebox/utils/KeyboardUtil.kt
+++ app/src/main/java/com/dinhlam/sharebox/utils/KeyboardUtil.kt
@@ -3,36 +3,27 @@ package com.dinhlam.sharebox.utils
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
-import android.os.Build
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import com.dinhlam.sharebox.extensions.getSystemServiceCompat
 
 object KeyboardUtil {
 
     fun hideKeyboard(view: View) {
-        val imm = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            view.context.getSystemService(InputMethodManager::class.java)
-        } else {
-            view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        }
+        val imm =
+            view.context.getSystemServiceCompat<InputMethodManager>(Context.INPUT_METHOD_SERVICE)
         imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     fun copyTextToClipboard(context: Context, text: String) {
-        val clipboardManager = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            context.getSystemService(ClipboardManager::class.java)
-        } else {
-            context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        }
+        val clipboardManager =
+            context.getSystemServiceCompat<ClipboardManager>(Context.CLIPBOARD_SERVICE)
         clipboardManager.setPrimaryClip(ClipData.newPlainText("share_box_recovery_password", text))
     }
 
     fun getTextFromClipboard(context: Context): String {
-        val clipboardManager = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            context.getSystemService(ClipboardManager::class.java)
-        } else {
-            context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        }
+        val clipboardManager =
+            context.getSystemServiceCompat<ClipboardManager>(Context.CLIPBOARD_SERVICE)
         val primaryClip = clipboardManager.primaryClip ?: return ""
         return if (primaryClip.itemCount > 0) {
             primaryClip.getItemAt(0).text.toString()
