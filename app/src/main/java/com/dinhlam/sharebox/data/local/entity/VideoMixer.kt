f89f44a111e6d7923d1aaf753e6add51260ca9ff
Dinh Lam <dinhlamvn353@gmail.com>
2023-05-23 09:52:49 +0700

    add trending score

--- app/src/main/java/com/dinhlam/sharebox/data/local/entity/VideoMixer.kt
+++ app/src/main/java/com/dinhlam/sharebox/data/local/entity/VideoMixer.kt
@@ -20,8 +20,10 @@ data class VideoMixer(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "share_id") val shareId: String,
     @ColumnInfo(name = "original_url") val originalUrl: String,
-    @ColumnInfo(name = "source") val videoSource: VideoSource,
+    @ColumnInfo(name = "source") val source: VideoSource,
+    @ColumnInfo(name = "source_id") val sourceId: String,
     @ColumnInfo(name = "cdn_url") val cdnUrl: String?,
+    @ColumnInfo(name = "trending_score") val trendingScore: Int = 0,
     @ColumnInfo(name = "created_at") val createdAt: Long = System.currentTimeMillis(),
     @ColumnInfo(name = "updated_at") val updatedAt: Long = System.currentTimeMillis()
 )
